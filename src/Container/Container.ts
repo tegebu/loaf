ec776fe7ae58ce3d2127f06b467d4b6fd105690c
jamashita <jamashita@arachnid.xyz>
2020-10-06 21:13:53 +0900

    feat: 🎸 file injection

--- src/Container/Container.ts
+++ src/Container/Container.ts
@@ -1,4 +1,6 @@
 import { Container } from 'inversify';
+import { File } from '../General/File';
+import { IFile } from '../General/Interface/IFile';
 import { ILogger } from '../Infrastructure/Interface/ILogger';
 import { logger } from '../Infrastructure/Logger';
 import { Types } from './Types';
@@ -6,6 +8,7 @@ import { Types } from './Types';
 const c: Container = new Container();
 
 // Infrastructure
+c.bind<IFile>(Types.File).toConstantValue(new File());
 c.bind<ILogger>(Types.Logger).toConstantValue(logger);
 
 export const container: Container = c;
--- src/Container/Types.ts
+++ src/Container/Types.ts
@@ -1,4 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/typedef
 export const Types = {
+  File: Symbol(),
   Logger: Symbol()
 } as const;
