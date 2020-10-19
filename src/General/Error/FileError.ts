9ea5ac3d8fbea403c0754f28dbef87f7ac322b6a
jamashita <jamashita@arachnid.xyz>
2020-10-19 15:48:33 +0900

    feat: 🎸 error update

--- src/General/Error/FileError.ts
+++ src/General/Error/FileError.ts
@@ -1,8 +1,6 @@
 import { DataSourceError } from '@jamashita/publikum-error';
 
 export class FileError extends DataSourceError<'FileError', 'File'> {
-  public readonly noun: 'FileError' = 'FileError';
-
   public constructor(message: string, cause?: NodeJS.ErrnoException) {
     super('FileError', 'File', message, cause);
   }
