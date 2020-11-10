1dc4aff004ca2dbc01f9479c45dbf6300d8370c8
jamashita <jamashita@arachnid.xyz>
2020-11-10 22:25:25 +0900

    feat: 🎸 Interactor implementation done

--- src/Interactor/Interface/ITegeInteractor.ts
+++ src/Interactor/Interface/ITegeInteractor.ts
@@ -1,9 +1,13 @@
 import { DataSourceError } from '@jamashita/publikum-error';
 import { Noun } from '@jamashita/publikum-interface';
 import { Superposition } from '@jamashita/publikum-monad';
-import { TegeError, Teges } from '@tegebu/syrup';
+import { Tege, TegeError, Teges } from '@tegebu/syrup';
 
-// TODO APPEND, UPDATE, DELETE_ONE
 export interface ITegeInteractor extends Noun<'TegeInteractor'> {
+  create(tege: Tege): Superposition<unknown, TegeError | DataSourceError>;
+
+  // TODO
+  // update(tege: Tege): Superposition<unknown, TegeError | DataSourceError>;
+
   save(teges: Teges): Superposition<unknown, TegeError | DataSourceError>;
 }
