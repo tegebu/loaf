2f9730eeb25a53a6bd0e5d25473f02248d9ff11d
jamashita <jamashita@arachnid.xyz>
2020-11-12 00:01:43 +0900

    feat: 🎸 Controller done

--- src/Controller/API/__tests__/TegeController.spec.ts
+++ src/Controller/API/__tests__/TegeController.spec.ts
@@ -1,3 +1,4 @@
+import { DataSourceError, MockDataSourceError } from '@jamashita/publikum-error';
 import { Superposition } from '@jamashita/publikum-monad';
 import { TegeError } from '@tegebu/syrup';
 import express, { Express } from 'express';
@@ -13,7 +14,7 @@ import { TegeController } from '../TegeController';
 
 describe('TegeController', () => {
   describe('POST /', () => {
-    it('returns CREATED', async () => {
+    it('responds CREATED', async () => {
       expect.assertions(1);
 
       const tegeInteractor: ITegeInteractor = container.get<ITegeInteractor>(Types.TegeInteractor);
@@ -47,6 +48,114 @@ describe('TegeController', () => {
       });
 
       expect(response.status).toBe(StatusCodes.CREATED);
+    });
+
+    it('responds BAD_REQUEST when given body was not convertable to TegeInputJSON', async () => {
+      expect.assertions(1);
+
+      const tegeInteractor: ITegeInteractor = container.get<ITegeInteractor>(Types.TegeInteractor);
+
+      const stub1: SinonStub = sinon.stub();
+
+      tegeInteractor.create = stub1;
+      stub1.returns(Superposition.alive<unknown, TegeError>(null));
+
+      const app: Express = express();
+
+      useContainer(container);
+      app.use(express.urlencoded({
+        extended: false
+      }));
+      app.use(express.json());
+      useExpressServer<Express>(app, {
+        controllers: [TegeController]
+      });
+
+      const response: supertest.Response = await supertest(app).post('/teges').send({
+        name: null,
+        playingTime: 30,
+        players: {
+          type: 'unique',
+          value: 4
+        },
+        minAge: 12,
+        imagePath: '/root',
+        expansion: false
+      });
+
+      expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+    });
+
+    it('responds INTERNAL_SERVER_ERROR when tegeInteractor.create() returns Dead.TegeError', async () => {
+      expect.assertions(1);
+
+      const tegeInteractor: ITegeInteractor = container.get<ITegeInteractor>(Types.TegeInteractor);
+
+      const stub1: SinonStub = sinon.stub();
+
+      tegeInteractor.create = stub1;
+      stub1.returns(Superposition.dead<unknown, TegeError>(new TegeError('test failed')));
+
+      const app: Express = express();
+
+      useContainer(container);
+      app.use(express.urlencoded({
+        extended: false
+      }));
+      app.use(express.json());
+      useExpressServer<Express>(app, {
+        controllers: [TegeController]
+      });
+
+      const response: supertest.Response = await supertest(app).post('/teges').send({
+        name: 'monopoly',
+        playingTime: 30,
+        players: {
+          type: 'unique',
+          value: 4
+        },
+        minAge: 12,
+        imagePath: '/root',
+        expansion: false
+      });
+
+      expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+    });
+
+    it('responds INTERNAL_SERVER_ERROR when  tegeInteractor.create() returns Dead.DataSourceError', async () => {
+      expect.assertions(1);
+
+      const tegeInteractor: ITegeInteractor = container.get<ITegeInteractor>(Types.TegeInteractor);
+
+      const stub1: SinonStub = sinon.stub();
+
+      tegeInteractor.create = stub1;
+      stub1.returns(Superposition.dead<unknown, DataSourceError>(new MockDataSourceError()));
+
+      const app: Express = express();
+
+      useContainer(container);
+      app.use(express.urlencoded({
+        extended: false
+      }));
+      app.use(express.json());
+      useExpressServer<Express>(app, {
+        controllers: [TegeController]
+      });
+
+      const response: supertest.Response = await supertest(app).post('/teges').send({
+        name: 'monopoly',
+        playingTime: 30,
+        players: {
+          type: 'unique',
+          value: 4
+        },
+        minAge: 12,
+        imagePath: '/root',
+        expansion: false
+      });
+
+      expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
     });
   });
 });
