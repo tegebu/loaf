a8db836280be9d6b9ad3676532c937e70d11fd8c
jamashita <jamashita@arachnid.xyz>
2020-11-12 20:07:38 +0900

    feat: 🎸 moved

--- src/General/File/File.ts
+++ src/General/File/File.ts
@@ -20,7 +20,7 @@ export class File implements IFile {
 
   public async write(path: string, data: string): Promise<void> {
     try {
-      return await fs.promises.writeFile(path, data);
+      await fs.promises.writeFile(path, data); return;
     }
     catch (err: unknown) {
       if (err instanceof Error) {
@@ -33,7 +33,7 @@ export class File implements IFile {
 
   public async delete(path: string): Promise<void> {
     try {
-      return await fs.promises.unlink(path);
+      await fs.promises.unlink(path); return;
     }
     catch (err: unknown) {
       if (err instanceof Error) {
@@ -46,7 +46,7 @@ export class File implements IFile {
 
   public async copy(src: string, dest: string): Promise<void> {
     try {
-      return await fs.promises.copyFile(src, dest);
+      await fs.promises.copyFile(src, dest); return;
     }
     catch (err: unknown) {
       if (err instanceof Error) {
