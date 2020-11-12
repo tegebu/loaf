d89db0fd440104e82c37475ff4cd538056fb7485
jamashita <jamashita@arachnid.xyz>
2020-11-12 22:25:51 +0900

    feat: 🎸 git.commit()

--- src/General/Git/Git.ts
+++ src/General/Git/Git.ts
@@ -8,7 +8,8 @@ const git: SimpleGit = gitP();
 export class Git implements IGit {
   public async add(files: Array<string>): Promise<void> {
     try {
-      await git.add(files); return;
+      await git.add(files);
+      return;
     }
     catch (err: unknown) {
       if (err instanceof GError) {
@@ -17,6 +18,22 @@ export class Git implements IGit {
 
       throw err;
     }
+  }
+
+  public async commit(message: string): Promise<void> {
+    try {
+      await git.commit(message);
+
+      return;
+    }
+    catch (err: unknown) {
+      if (err instanceof GError) {
+        throw new GitError(err.message, err);
+      }
+
+      throw err;
+    }
+
   }
 
   public async push(remote: string, branch: string): Promise<void> {
